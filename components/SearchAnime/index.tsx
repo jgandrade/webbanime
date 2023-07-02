bea5eee45914bc7a77a37d2ac00465c170421afa
jgandrade <johnglenn.andrade@gmail.com>
2023-07-03 07:29:08 +0800

    [search-anime] added search form for landing page and added search functionality

--- components/SearchAnime/index.tsx
+++ components/SearchAnime/index.tsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const SearchAnime = () => {
   const [query, setQuery] = useState<string>("");
-
+  const router = useRouter();
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setQuery(value);
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    router.push(`/search/${query}`);
+  };
+
   return (
-    <form>
+    <form onSubmit={(event) => handleSubmit(event)}>
       <input value={query} onChange={(event) => handleChange(event)} />
+      <button type="submit">Search</button>
     </form>
   );
 };
