7da5626a957573fe4b261fae824c6d69471edc72
jgandrade <johnglenn.andrade@gmail.com>
2023-07-03 12:47:14 +0800

    [watch-anime] Improved UI and fixed bugs

--- components/SearchAnime/index.tsx
+++ components/SearchAnime/index.tsx
@@ -14,14 +14,23 @@ const SearchAnime = () => {
   };
 
   return (
-    <form onSubmit={(event) => handleSubmit(event)}>
+    <form
+      onSubmit={(event) => handleSubmit(event)}
+      className="flex items-center"
+    >
       <input
         type="text"
         placeholder="Search Anime"
         value={query}
         onChange={(event) => handleChange(event.target.value)}
+        className="px-4 py-2 rounded-l-lg focus:outline-none focus:ring focus:border-blue-300 bg-gray-900 text-gray-200"
       />
-      <button type="submit">Search</button>
+      <button
+        type="submit"
+        className="px-4 py-2 rounded-r-lg bg-gray-700 text-white"
+      >
+        Search
+      </button>
     </form>
   );
 };
