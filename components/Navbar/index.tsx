9924209a4aece5131569be190fce41f687ad9744
jgandrade <johnglenn.andrade@gmail.com>
2023-08-26 18:03:40 +0800

    [fix-logic] fix responsiveness

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -10,15 +10,33 @@ import {
 import { auth, db } from "@/firebase/firebaseConfig";
 import { setDoc, doc, getDoc } from "firebase/firestore";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setUserDetails } from "@/redux/userSlice";
 import useDetails from "@/hooks/useDetails";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import GoogleIcon from "@mui/icons-material/Google";
+import { useRouter } from "next/navigation";
+import { Search } from "@mui/icons-material";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const { name, photo, id } = useDetails();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [query, setQuery] = useState<string>("");
+  const router = useRouter();
+  const handleChange = (value: string) => {
+    setQuery(value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setMobileMenuOpen(false);
+    router.push(`/search/${query}`);
+  };
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
 
   function logout() {
     signOut(auth)
@@ -122,15 +140,89 @@ const Navbar = () => {
           WebbAnime
         </Typography>
       </Link>
-      <SearchAnime />
-      {name ? (
-        <Box className="flex gap-3 justify-center items-center">
-          {id && (
-            <Link href="/favorites">
-              <FavoriteIcon className="text-[#ff5722] cursor-pointer" />
-            </Link>
+      <Box className="hidden md:flex">
+        <SearchAnime />
+      </Box>
+      <button
+        className="bg-[#ff5722] hover:bg-[#db603a] text-white font-bold py-2 px-4 rounded-full flex items-center md:hidden"
+        onClick={toggleMobileMenu}
+      >
+        ☰
+      </button>
+      {mobileMenuOpen && (
+        <div className="flex flex-col items-center justify-center fixed top-0 right-0 h-full bg-[#333333] w-full p-4 transform translate-x-0 transition-transform duration-300 ease-in-out md:hidden">
+          <button
+            className="absolute top-2 right-2 text-white text-3xl"
+            onClick={toggleMobileMenu}
+          >
+            ✕
+          </button>
+          <Box className="relative items-center">
+            <form
+              onSubmit={(event) => handleSubmit(event)}
+              className="flex items-center"
+            >
+              <input
+                type="text"
+                placeholder="Search Anime"
+                value={query}
+                onChange={(event) => handleChange(event.target.value)}
+                spellCheck={false}
+              />
+              <button
+                className="absolute right-3 top-[10px] text-[#DC2626]"
+                type="submit"
+              >
+                <Search />
+              </button>
+            </form>
+          </Box>
+          {name ? (
+            <Box className="flex flex-col gap-3 justify-center items-center mt-5">
+              <Link href="/favorites" onClick={() => setMobileMenuOpen(false)}>
+                <button className="bg-[#ff5722] hover:bg-[#db603a] cursor-pointer text-white font-bold py-2 px-4 rounded-full flex items-center">
+                  Favorites <FavoriteIcon />
+                </button>
+              </Link>
+              {photo && (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  src={photo}
+                  alt="profile-pic"
+                  className="rounded-full mr-2"
+                  width={50}
+                />
+              )}
+              <button
+                className="bg-[#ff5722] hover:bg-[#db603a] text-white font-bold py-2 px-4 rounded-full flex items-center"
+                onClick={() => {
+                  logout();
+                  setMobileMenuOpen(false);
+                }}
+              >
+                Logout
+              </button>
+            </Box>
+          ) : (
+            <button
+              className="bg-[#ff5722] hover:bg-[#db603a] text-white font-bold py-2 px-4 rounded-full flex items-center"
+              onClick={() => {
+                login();
+                setMobileMenuOpen(false);
+              }}
+            >
+              Login with <GoogleIcon className="ml-2" />
+            </button>
           )}
+        </div>
+      )}
+      {name ? (
+        <Box className="gap-3 justify-center items-center hidden md:flex">
+          <Link href="/favorites">
+            <FavoriteIcon className="text-[#ff5722] cursor-pointer" />
+          </Link>
           {photo && (
+            // eslint-disable-next-line @next/next/no-img-element
             <img
               src={photo}
               alt="profile-pic"
